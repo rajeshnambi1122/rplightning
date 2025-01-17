26945e2553cc37ddd7948bf70f48257af22a172f
Rajesh Nambi <rajeshnambi2016@gmail.com>
2025-01-17 11:43:46 +0530

    updates

--- src/app/mine/mine.component.ts
+++ src/app/mine/mine.component.ts
@@ -52,7 +52,8 @@ export class MineComponent {
     // Wait for DOM to be ready before initializing TonConnect
     setTimeout(() => {
       this.tonConnect = new TonConnectUI({
-        manifestUrl: 'https://your-app-url.com/tonconnect-manifest.json',
+        manifestUrl:
+          'https://rajeshnambi1122.github.io/rplightning/tonconnect-manifest.json',
         buttonRootId: 'ton-connect-button',
       });
 
