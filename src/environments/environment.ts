e27d18c1292211a33054743964c5c178d6bb8467
Rajesh Nambi <rajeshnambi2016@gmail.com>
2025-02-10 21:28:52 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
-    production: true,
-     apiurl: 'https://9867-2405-201-e003-11db-f96a-ac25-e8f1-62b0.ngrok-free.app/',
-    //  webHook: 'https://b1cb-2405-201-e003-11db-18bc-7127-1bce-9e8f.ngrok-free.app/webhook/'
-  };
-  
+  production: true,
+  apiurl: 'https://dc32-2405-201-e003-11db-35c0-da9e-6dc9-9dc2.ngrok-free.app/',
+  //  webHook: 'https://b1cb-2405-201-e003-11db-18bc-7127-1bce-9e8f.ngrok-free.app/webhook/'
+};
